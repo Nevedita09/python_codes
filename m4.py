60f5e7cc31f4ac2d092a9e09626aba12848624e5
Nevedita09 <nevedita0901@gmail.com>
2025-05-03 22:03:37 +0530

    m4.py

---
+++ m4.py
@@ -0,0 +1,20 @@
+import list1
+import set2
+import dict3
+
+# List operations
+list1.append1("Python")
+list1.extend1(["AI", "ML"])
+list1.pop()
+list1.remove1("AI")
+
+# Set operations
+set2.adds2(5)
+set2.remove2(3)
+set2.slen2(set2.my_set)
+
+# Dict operations
+dict3.add3("A", 1)
+dict3.keys3()
+dict3.values3()
+dict3.len3(dict3.my_dict)
