f9eb7a34d65ad93b73a0ca4f27ffaead7ecdf6bb
Nevedita09 <nevedita0901@gmail.com>
2025-05-03 21:53:44 +0530

    Create Four Modules: list1.py, set2.py, dict3.py, m4.py

---
+++ Create Four Modules: list1.py, set2.py, dict3.py, m4.py
@@ -0,0 +1,23 @@
+# list1.py
+my_list = []
+
+def append1(x):
+    my_list.append(x)
+    print("Appended:", x)
+
+def extend1(l):
+    my_list.extend(l)
+    print("Extended with:", l)
+
+def pop():
+    if my_list:
+        print("Popped:", my_list.pop())
+    else:
+        print("List is empty")
+
+def remove1(x):
+    if x in my_list:
+        my_list.remove(x)
+        print("Removed:", x)
+    else:
+        print("Value not found in list")
